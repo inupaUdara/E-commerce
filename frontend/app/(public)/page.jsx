eaac15bde25ccb473187fdba77d54dc77660f094
Inupa Udara <127815279+inupaUdara@users.noreply.github.com>
2026-03-15 23:11:45 +0530

    feat: Comment out Newsletter component in Home page

--- frontend/app/(public)/page.jsx
+++ frontend/app/(public)/page.jsx
@@ -14,7 +14,7 @@ export default function Home() {
             <FollowedStoreProducts />
             <BestSelling />
             <OurSpecs />
-            <Newsletter />
+            {/* <Newsletter /> */}
         </div>
     );
 }
